b74d89d75cdf73295089c4ae827fe1eae302bc65
sshacker <shivamsonkar260@gmail.com>
2019-09-21 03:26:43 +0530

    added FACEBOOK LOGIN and GOOGLE SEARCH and YOUTUBE SEARCH with PLAY AND PAUSE and PROGRAM OPENING all working fine

--- jarvis_functions/google.py
+++ jarvis_functions/google.py
@@ -4,74 +4,65 @@ from jarvis_functions import speaker
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-def youtube(query):
+def google(query):
     """
-		TASK TO YOUTUBE SEARCH
-	"""
-    # create a new Chrome session
+        TASK TO GOOGLE SEARCH 
+    """
+    # create a new Firefox session
     driver = webdriver.Chrome()
+    driver.implicitly_wait(30)
     driver.maximize_window()
     
     # Navigate to the application home page
-    driver.get("http://www.youtube.com")
+    driver.get("http://www.google.com")
     
     # get the search textbox
-    search_field = driver.find_element_by_name("search_query")
+    search_field = driver.find_element_by_name("q")
     search_field.clear()
-
+    
+    to_search = query.split("SEARCH")[1].strip()
+    
     #user input query
     #user_search_query=input("enter search query :")
-    
-    to_search=""
-    if "PLAY" in query:
-        to_search = query.split("PLAY")[1].strip()
-    elif "SEARCH" in query:
-        to_search = query.split("SEARCH")[1].strip()
-    
+
     # enter search keyword and submit
     search_field.send_keys(to_search)
     search_field.submit()
     
     # get the list of elements which are displayed after the search
     # currently on result page using find_elements_by_class_name method
-    lists= driver.find_elements_by_id("video-title")
+    lists= driver.find_elements_by_class_name("ellip")
 
     # get the number of elements found
     print ("Found " + str(len(lists)) + " searches:")
 
-    current_page_url = driver.current_url
-    print(current_page_url)
+    # iterate through each element and print the text that is
+    # name of the search
 
     i=0
     for listitem in lists:
-        item=listitem.get_attribute("innerHTML")
+        item = listitem.get_attribute("innerHTML")
         print (str(i+1)+".",item)
         speaker.speak(item)
         i=i+1
-        if(i>=3):
-            print("... and many more search results")
+        if(i>=5):
+            print(".... and many more search results")
             break
+
             
-    info = "which item number would you like to play"
+    info = "which item number would you like to open"
     print(info)
     speaker.speak(info)
     
     i=int(input("enter item number : "))
     lists[i-1].click()
     
-    info="start playing..."
+    info="start opening..."
     print(info)
     speaker.speak(info)
     
-    #player
-    q = "'PLAY"
-    while q.upper() != "STOP":
-        q = input("enter : ")
-        if q.upper() == "PLAY" or q.upper() == "PAUSE":
-            p=driver.find_elements_by_xpath('//*[@id="player"]')
-            p[0].click()
-    
+    input("HALT")
     # close the browser window
     driver.quit()
-    
+ 
     return
